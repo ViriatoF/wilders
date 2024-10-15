0303d339d89dc6b21ee3b248f1284d6bba7fc18f
ViriatoF <viriato.ferreira44@gmail.com>
2024-10-15 11:26:47 +0200

    Each users with different color done

---
+++ script.js
@@ -0,0 +1,5 @@
+import chalk from "chalk";
+console.log(chalk.red("Vito"));
+console.log(chalk.blueBright("Lucas"));
+console.log(chalk.yellow("Julien"));
+console.log(chalk.greenBright("Timoteï"));
